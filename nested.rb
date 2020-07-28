04b084db5d01dee14e5baae7ae2fa1726c68161b
Pablo Menz <p.menz.r@gmail.com>
2020-07-28 05:04:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def adding_matz
           :languages => ["C"]
         },
         :yukihiro_matsumoto => {
-          :known_for => "Ruby"
+          :known_for => "Ruby",
           :languages => ["LISP", "C"]
-        },
+        }
  		}
      
 
